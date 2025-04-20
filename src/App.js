d738a57967f19812c46b5ae9b4cf00ae2463408d
asiel <br630455@gmail.com>
2025-04-20 16:10:43 -0600

    0.0.3 Prueba sobre mi

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from "react";
 import Headers from "./views/Header";
+import Sobremi from "./views/aboutme"
 
 function App() {
   return (
       <div>
         <Headers/>
+        <Sobremi/>
       </div>
   );
 }
--- src/views/aboutme.js
+++ src/views/aboutme.js
@@ -1,8 +1,12 @@
 import React from "react";
+import './aboutme.css';
 
 function Sobremi(){
-    <div>
-        
+    return(
+    <div className="content">
+        <h1 className="subtitle_one">SOBRE MI</h1>
+        <p className="description">HOLAAAAASJKSAKKJSAKSAJDKASKDALKDAWMDMAM</p>
     </div>
+    )
 }
 export default Sobremi;
