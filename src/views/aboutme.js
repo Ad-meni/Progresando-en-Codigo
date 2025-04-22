4eb5cf4996ff19d356e7655ee4c51a4876efb9bb
asiel <br630455@gmail.com>
2025-04-21 21:59:16 -0600

    0.0.51 Actualizacion de el 'sobre mi'

--- src/views/aboutme.js
+++ src/views/aboutme.js
@@ -5,9 +5,17 @@ function Sobremi(){
     return(
     <div className="content">
         <h1 className="subtitle_one">SOBRE MI</h1>
-        <p className="description">Soy Asiel Guevara Reyes, aunque mis amigos me llaman simplemente Asiel. He aprendido a ver el mundo desde una perspectiva lógica, pero sin perder mi esencia. Aunque soy un poco tímido y tengo mis miedos y dudas, siempre busco maneras de mejorar y encontrar mi lugar. Descubrí mi pasión por la programación desde pequeño, fascinado por cómo se crean aplicaciones y juegos. Hoy, estudio desarrollo de software multiplataforma y dedico mis días a aprender más sobre backend, bases de datos como SQL, C#, PHP, y React, mientras desarrollo mi proyecto actual.
-Aunque no soy fan del IoT, me siento en casa dentro del mundo del código, donde cada línea me permite construir y crear algo nuevo. También disfruto escribir, lo cual me ayuda a organizar mis pensamientos y explorar mi creatividad, y espero algún día compartir mis escritos. Mi meta es crecer como programador y vivir de esta pasión, mientras me sigo retando a aprender cosas nuevas, como empezar en el gimnasio y superar mis propias barreras. Este soy yo, siempre en progreso, siempre buscando avanzar.
-</p>
+        <p className="description">Soy Asiel Guevara Reyes, aunque mis amigos me llaman simplemente Asiel.
+              He aprendido a ver el mundo desde una perspectiva lógica, pero sin perder mi esencia. Aunque soy un poco tímido
+              y tengo mis miedos y dudas, siempre busco maneras de mejorar y encontrar mi lugar. Descubrí mi pasión por la 
+              programación desde pequeño, fascinado por cómo se crean aplicaciones y juegos. Hoy, estudio desarrollo de software 
+              multiplataforma y dedico mis días a aprender más sobre backend, bases de datos como SQL, C#, PHP, y React, mientras 
+              desarrollo mi proyecto actual.Aunque no soy fan del IoT, me siento en casa dentro del mundo del código, donde cada 
+              línea me permite construir y crear algo nuevo. También disfruto escribir, lo cual me ayuda a organizar mis 
+              pensamientos y explorar mi creatividad, y espero algún día compartir mis escritos. Mi meta es crecer como 
+              programador y vivir de esta pasión, mientras me sigo retando a aprender cosas nuevas, como empezar en el gimnasio y 
+              superar mis propias barreras. Este soy yo, siempre en progreso, siempre buscando avanzar.
+        </p>
     </div>
     )
 }
